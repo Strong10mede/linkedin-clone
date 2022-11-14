2c410d14ef628eb5c226ab31ae674a0c3665939c
Strong10mede <kmayur809@gmail.com>
2022-11-14 16:03:53 +0530

    post done

---
+++ src/Components/Feed/Post/Post.js
@@ -0,0 +1,35 @@
+import React, { forwardRef } from "react";
+import "./Post.css";
+import ThumbUpAltOutlinedIcon from "@mui/icons-material/ThumbUpAltOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
+import { Avatar } from "@material-ui/core";
+import InputOptions from "../InputOptions/InputOptions";
+const Post = forwardRef(({ name, description, message, photoUrl }, ref) => {
+  return (
+    <div ref={ref} className="post">
+      <div className="post__header">
+        <Avatar src={photoUrl}>{name[0]}</Avatar>
+
+        <div className="post__info">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+
+      <div className="post__body">
+        <p>{message}</p>
+      </div>
+
+      <div className="post__buttons">
+        <InputOptions Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray" />
+        <InputOptions Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+        <InputOptions Icon={ShareOutlinedIcon} title="Share" color="gray" />
+        <InputOptions Icon={SendOutlinedIcon} title="Send" color="gray" />
+      </div>
+    </div>
+  );
+});
+
+export default Post;
