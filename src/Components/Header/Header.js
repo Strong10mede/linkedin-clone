1256a3839b5525dbea3ffc74cdbf25912031947e
Strong10mede <kmayur809@gmail.com>
2022-11-14 12:18:26 +0530

    header.js partial done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "../HeaderOption/HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+function Header() {
+  const logOutOfApp = () => {};
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png?20140125013055"
+          alt="Linkedin Logo"
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          onClick={logOutOfApp}
+          title={user?.displayName}
+          avatar={true}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
