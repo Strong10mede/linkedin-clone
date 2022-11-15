012a5a20ec881627026e03160b1e1c3b81f07e9f
Strong10mede <kmayur809@gmail.com>
2022-11-15 11:24:48 +0530

    widgets done

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./Feed.css";
+import FlipMove from "react-flip-move";
+import firebase from "firebase/compat/app" 
 import { db } from "../../firebase";
+import CreateIcon from "@mui/icons-material/Create";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
+import InputOptions from "./InputOptions/InputOptions";
+import Post from "./Post/Post";
 function Feed() {
   const [posts, setPosts] = useState([]);
   const [input, setInput] = useState("");
@@ -20,8 +29,68 @@ function Feed() {
           )
         ),
     []
-  );
-  return <div className="feed"></div>;
+    );
+    
+    const sendPost = (e) => {
+        e.preventDefault();
+        db.collection("posts").add({
+            name: user?.displayName,
+            description: user?.email,
+            message: input,
+            photoUrl: user?.photoUrl,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+
+        setInput("");
+    }
+    return (
+      <div className="feed">
+        <div className="feed__inputContainer">
+          <div className="feed__input">
+            <CreateIcon />
+            <form>
+              <input
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+              />
+              <button onClick={sendPost} type="submit">
+                Send
+              </button>
+            </form>
+          </div>
+
+          <div className="feed__inputOptions">
+            <InputOptions title="Photo" Icon={ImageIcon} color="#70B5F9" />
+            <InputOptions
+              title="Video"
+              Icon={SubscriptionsIcon}
+              color="#E7A33E"
+            />
+            <InputOptions title="Event" Icon={EventNoteIcon} color="#C0CBCD" />
+            <InputOptions
+              title="Write article"
+              Icon={CalendarViewDayIcon}
+              color="#7FC15E"
+            />
+          </div>
+        </div>
+
+        <FlipMove>
+          {posts.map(
+            ({ id, data: { name, description, message, photoUrl } }) => (
+              <Post
+                key={id}
+                name={name}
+                description={description}
+                message={message}
+                photoUrl={photoUrl}
+              />
+            )
+          )}
+        </FlipMove>
+      </div>
+    );
 }
 
 export default Feed;
---
+++ src/Components/Widgets/Widgets.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Widgets.css";
+import InfoIcon from "@mui/icons-material/Info";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+function Widgets() {
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecordIcon />
+      </div>
+      <div className="widgets__articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+  return (
+    <div className="widgets">
+      <div className="widgets__header">
+        <h2>Linkedin News</h2>
+        <InfoIcon />
+      </div>
+      {newsArticle("The Full Stack Junkie Is Back", "Top news - 9099 reader")}
+      {newsArticle("Coronavirus: UK updates", "Top news - 886 reader")}
+      {newsArticle("Bitcoin Breaks $22k", "Crypto - 8000 reader")}
+      {newsArticle("JavaScript Mastery", "Code - 120000 reader")}
+      {newsArticle("Tesla hits new highs", "Cars & auto - 300 reader")}
+    </div>
+  );
+}
+
+export default Widgets;
