c1639ca424079980010c9c39a12f650e27e23002
Strong10mede <kmayur809@gmail.com>
2022-11-21 11:46:26 +0530

    app done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
+import React, { useEffect } from "react";
 import "./App.css";
+import Feed from "./Components/Feed/Feed";
+import Header from "./Components/Header/Header";
 import Login from "./Components/Login/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Widgets from "./Components/Widgets/Widgets";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
+
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    //eslint-disable-next-line
+  }, []);
   return (
     <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </Router>
+      <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          <Widgets />
+        </div>
+      )}
     </div>
   );
 }
