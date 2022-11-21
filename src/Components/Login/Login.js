11721da61c99928d54f63fbafd0b0219c318b80d
Strong10mede <kmayur809@gmail.com>
2022-11-21 12:46:42 +0530

    Header.css done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -53,50 +53,48 @@ function Login() {
   };
   return (
     <div className="login">
-      <div className="login">
-        <img
-          src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png"
-          alt="Linkedin main logo"
+      <img
+        src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png"
+        alt="Linkedin main logo"
+      />
+
+      <form>
+        <input
+          placeholder="Full name (required for register)"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
 
-        <form>
-          <input
-            placeholder="Full name (required for register)"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+        <input
+          placeholder="Profile pic URL (optional)"
+          type="text"
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
+        />
 
-          <input
-            placeholder="Profile pic URL (optional)"
-            type="text"
-            value={profilePic}
-            onChange={(e) => setProfilePic(e.target.value)}
-          />
-
-          <input
-            placeholder="Email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            placeholder="Password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit" onClick={loginHandler}>
-            Sign In
-          </button>
-        </form>
-        <p>
-          Not a member?{" "}
-          <span className="login__register" onClick={register}>
-            Register Now
-          </span>
-        </p>
-      </div>
+        <input
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" onClick={loginHandler}>
+          Sign In
+        </button>
+      </form>
+      <p>
+        Not a member?{" "}
+        <span className="login__register" onClick={register}>
+          Register Now
+        </span>
+      </p>
     </div>
   );
 }
