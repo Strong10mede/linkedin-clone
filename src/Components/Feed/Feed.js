f80b333dd7886d7dcf90943f602e95fc2a5c480b
Strong10mede <kmayur809@gmail.com>
2022-11-21 11:26:25 +0530

    feed redux part done

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Feed.css";
 import FlipMove from "react-flip-move";
-import firebase from "firebase/compat/app" 
+import firebase from "firebase/compat/app";
 import { db } from "../../firebase";
 import CreateIcon from "@mui/icons-material/Create";
 import ImageIcon from "@mui/icons-material/Image";
@@ -10,10 +10,12 @@ import EventNoteIcon from "@mui/icons-material/EventNote";
 import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 import InputOptions from "./InputOptions/InputOptions";
 import Post from "./Post/Post";
+import { selectUser } from "../../features/userSlice";
+import { useSelector } from "react-redux";
 function Feed() {
   const [posts, setPosts] = useState([]);
   const [input, setInput] = useState("");
-  const user = {};
+  const user = useSelector(selectUser);
 
   useEffect(
     () =>
@@ -29,68 +31,66 @@ function Feed() {
           )
         ),
     []
-    );
-    
-    const sendPost = (e) => {
-        e.preventDefault();
-        db.collection("posts").add({
-            name: user?.displayName,
-            description: user?.email,
-            message: input,
-            photoUrl: user?.photoUrl,
-            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-        });
+  );
 
-        setInput("");
-    }
-    return (
-      <div className="feed">
-        <div className="feed__inputContainer">
-          <div className="feed__input">
-            <CreateIcon />
-            <form>
-              <input
-                type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-              />
-              <button onClick={sendPost} type="submit">
-                Send
-              </button>
-            </form>
-          </div>
+  const sendPost = (e) => {
+    e.preventDefault();
+    db.collection("posts").add({
+      name: user?.displayName,
+      description: user?.email,
+      message: input,
+      photoUrl: user?.photoUrl,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
 
-          <div className="feed__inputOptions">
-            <InputOptions title="Photo" Icon={ImageIcon} color="#70B5F9" />
-            <InputOptions
-              title="Video"
-              Icon={SubscriptionsIcon}
-              color="#E7A33E"
+    setInput("");
+  };
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
             />
-            <InputOptions title="Event" Icon={EventNoteIcon} color="#C0CBCD" />
-            <InputOptions
-              title="Write article"
-              Icon={CalendarViewDayIcon}
-              color="#7FC15E"
-            />
-          </div>
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
+          </form>
         </div>
 
-        <FlipMove>
-          {posts.map(
-            ({ id, data: { name, description, message, photoUrl } }) => (
-              <Post
-                key={id}
-                name={name}
-                description={description}
-                message={message}
-                photoUrl={photoUrl}
-              />
-            )
-          )}
-        </FlipMove>
+        <div className="feed__inputOptions">
+          <InputOptions title="Photo" Icon={ImageIcon} color="#70B5F9" />
+          <InputOptions
+            title="Video"
+            Icon={SubscriptionsIcon}
+            color="#E7A33E"
+          />
+          <InputOptions title="Event" Icon={EventNoteIcon} color="#C0CBCD" />
+          <InputOptions
+            title="Write article"
+            Icon={CalendarViewDayIcon}
+            color="#7FC15E"
+          />
+        </div>
       </div>
-    );
+
+      <FlipMove>
+        {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+          <Post
+            key={id}
+            name={name}
+            description={description}
+            message={message}
+            photoUrl={photoUrl}
+          />
+        ))}
+      </FlipMove>
+    </div>
+  );
 }
 
 export default Feed;
