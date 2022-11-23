a664d48eea3d4a3f6faa37de6c47a13f6c8ba3c5
Strong10mede <kmayur809@gmail.com>
2022-11-23 11:45:56 +0530

    onAuthStateChanged

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    //onAuthStateChanged is used here to get the current user who is logged in.
     auth.onAuthStateChanged((userAuth) => {
       console.log("onAuthStateChanged App.js");
       if (userAuth) {
